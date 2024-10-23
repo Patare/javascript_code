c6226d9bacbf10c8e102532262fb52758357154a
Patare <kalyanpatare1999@gmail.com>
2024-10-23 12:48:22 +0530

    variable

---
+++ Basic/01_variable.js
@@ -0,0 +1,13 @@
+const accound_Id=125468;
+let accEmail ="kalyan@gmail.com";
+var accPass= 123456;
+
+
+// accound_Id=777;
+accEmail="patare@gmail.com";
+accPass=88888;
+
+console.table([
+     accound_Id 
+    ,accEmail ,
+    accPass])
